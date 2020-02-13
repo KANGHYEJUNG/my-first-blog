a9f55b68357ff95abff7c1e1c5e1aec574204621
hyejung <khjtwt@gmail.com>
2020-02-14 01:59:56 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), # 상세 페이지 url 만들기
+    path('post/new', views.post_new, name='post_new'), # 새 게시글 추가하기
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'), # 특정 게시글 수정하기
 ]
 
